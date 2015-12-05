38e9f65fe67c724ca00eb874231459bcc6e3c333
Andrew <nightmare.quake@Mail.ru>
2015-12-05 12:58:00 +0200

    Update install.yaourt.sh

--- tmp/install.yaourt.sh
+++ tmp/install.yaourt.sh
@@ -2,7 +2,7 @@
 
 function install() {
    mkdir /tmp/$2
-   wget $1 https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=package-query -O /tmp/$2/PKGBUILD
+   wget $1 -O /tmp/$2/PKGBUILD
    cd /tmp/$2
    makepkg
    echo 'cd /tmp/$2/; pacman -U packagename'
