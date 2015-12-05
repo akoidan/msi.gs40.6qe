134f37d57a7bf5f633ee9d0c4de2b92ae9d25738
root <root@localhost.localdomain>
2015-12-05 13:03:37 +0000

    aded executeme

---
+++ tmp/executeme.sh
@@ -0,0 +1,2 @@
+pacman -S vim mc wifi-menu dialog wpa_supplicant wpa_gui pkgfile dosfstools git gist openssh wget bash-completion yajl core/hdparm core/linux-firmware community/lshw
+sh /tmp/install.yaourt.sh
