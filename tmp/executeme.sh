7e0bc83e8e07a31e2078cfb0d1098c625e67c2f2
Andrew <nightmare.quake@Mail.ru>
2015-12-08 23:08:41 +0000

    Update executeme.sh

--- tmp/executeme.sh
+++ tmp/executeme.sh
@@ -1,2 +1,4 @@
-pacman -S vim mc wifi-menu dialog wpa_supplicant wpa_gui pkgfile dosfstools git gist openssh wget bash-completion yajl core/hdparm core/linux-firmware community/lshw
+pacman -S vim mc wifi-menu dialog wpa_supplicant wpa_gui pkgfile dosfstools git gist openssh wget bash-completion yajl core/hdparm core/linux-firmware community/lshw abs
+echo 'MAKEFLAGS="-j5"' > /etc/makepkg.conf
 sh /tmp/install.yaourt.sh
+echo 'run "abs" cp /tmp/kernel/ to  /var/abs/core/linux and run makepkg'
