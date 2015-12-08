0b24f9be984b00643dabb39412b1d92036d56070
Andrew <nightmare.quake@Mail.ru>
2015-12-08 23:22:02 +0000

    Update executeme.sh

--- tmp/executeme.sh
+++ tmp/executeme.sh
@@ -1,7 +1,7 @@
 echo 'uncomment multilib in /etc/pacman.conf'
 pacman -S vim mc wifi-menu dialog wpa_supplicant wpa_gui pkgfile dosfstools git gist openssh wget bash-completion yajl core/hdparm core/linux-firmware community/lshw abs
 #xorg
-pacman -S xf86-video-intel xorg-server xorg-server-utils plasma kde-applications
+pacman -S xf86-video-intel xorg-server xorg-server-utils plasma kde-applications sddm
 echo 'MAKEFLAGS="-j5"' > /etc/makepkg.conf
 sh /tmp/install.yaourt.sh
 echo 'run "abs" cp /tmp/kernel/ to  /var/abs/core/linux and run makepkg'
